d02389f4f34938395d70d9ee2d7116b9f59c556f
kaiguo <kaiguo@mogu.com>
2024-04-11 14:41:28 +0800

    feat: 第三方服务

---
+++ wkgmall-third-party/src/main/java/com/kaiguo/wkgmall/thirdparty/WkgThirdPartyApplication.java
@@ -0,0 +1,11 @@
+package com.kaiguo.wkgmall.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkgThirdPartyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WkgThirdPartyApplication.class, args);
+    }
+}
