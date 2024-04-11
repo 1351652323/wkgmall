e8a7ffe9550293dbe5e39f6a217f6d33fab664c4
kaiguo <kaiguo@mogu.com>
2024-04-11 21:19:53 +0800

    feat: 添加第三方路径thirdparty

--- wkgmall-third-party/src/main/java/com/kaiguo/wkgmall/thirdparty/controller/OssController.java
+++ wkgmall-third-party/src/main/java/com/kaiguo/wkgmall/thirdparty/controller/OssController.java
@@ -7,6 +7,7 @@ import com.aliyun.oss.model.PolicyConditions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
@@ -15,6 +16,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/thirdparty")
 public class OssController {
 
     @Autowired
