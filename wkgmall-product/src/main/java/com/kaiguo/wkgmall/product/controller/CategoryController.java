ed7d4fc91342b041333f8ee066b17532fbca949a
kaiguo <kaiguo@mogu.com>
2024-04-11 15:12:07 +0800

    feat: 菜单

--- wkgmall-product/src/main/java/com/kaiguo/wkgmall/product/controller/CategoryController.java
+++ wkgmall-product/src/main/java/com/kaiguo/wkgmall/product/controller/CategoryController.java
@@ -1,14 +1,11 @@
 package com.kaiguo.wkgmall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.kaiguo.wkgmall.product.entity.CategoryEntity;
 import com.kaiguo.wkgmall.product.service.CategoryService;
@@ -31,13 +28,13 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装起来
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    @GetMapping("/list/tree")
+    //@RequiresPermissions("product:category:list")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("data", entities);
     }
 
 
